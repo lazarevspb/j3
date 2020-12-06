5becb492a9f717485a3949d8874b9ebdee181b2c
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-12-06 22:27:02 +0300

    lesson6 final commit

--- lesson1/fruit/Apple.java
+++
@@ -1,7 +0,0 @@
-package lesson1.fruit;
-
-public class Apple extends Fruit {
-    public Apple() {
-        super.weight = 1.0f;
-    }
-}
--- lesson1/fruit/Orange.java
+++
@@ -1,7 +0,0 @@
-package lesson1.fruit;
-
-public class Orange extends Fruit {
-    public Orange() {
-        super.weight = 1.5f;
-    }
-}
