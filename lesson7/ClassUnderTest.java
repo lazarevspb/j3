38d6fc82544d6fde5ba3af4564a873e36a4400b0
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-12-11 21:12:35 +0300

    lesson7 first commit

---
+++ lesson7/ClassUnderTest.java
@@ -0,0 +1,8 @@
+package lesson7;
+
+public class ClassUnderTest {
+//    @BeforeSuite
+//    void
+
+
+}
---
+++ lesson7/PerformingTests.java
@@ -0,0 +1,9 @@
+package lesson7;
+
+
+public class PerformingTests {
+    public static void start(Class aClass){
+        
+    }
+    
+}
