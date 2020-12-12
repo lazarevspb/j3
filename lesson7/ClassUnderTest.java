96dd8a9b29f975c49325eca9925e55380b613f17
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-12-12 09:58:09 +0300

    lesson7 final commit

--- lesson7/ClassUnderTest.java
+++ lesson7/ClassUnderTest.java
@@ -1,8 +1,24 @@
 package lesson7;
 
+
 public class ClassUnderTest {
-//    @BeforeSuite
-//    void
+    @AfterSuite(priority = 1)
+    public static void methodAfterSuiteSuite() {
+        System.out.println("methodAfterSuiteSuite summoned");
+    }
 
+    @BeforeSuite(priority = 0)
+    public static void methodBeforeSuite() {
+        System.out.println("methodBeforeSuite summoned");
+    }
 
+    @Test(priority = 1)
+   static void justATestableMethod() {
+        System.out.println("justATestableMethod summoned");
+    }
+
+    @Test(priority = 2)
+    static  void justATestableMethodTwo() {
+        System.out.println("justATestableMethodTwo summoned");
+    }
 }
--- lesson7/PerformingTests.java
+++ lesson7/PerformingTests.java
@@ -1,9 +1,52 @@
 package lesson7;
 
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+/**
+ * Homework for lesson #7
+ *
+ * @author Valeriy Lazarev
+ * @since 12.12.2020
+ */
 public class PerformingTests {
-    public static void start(Class aClass){
-        
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        start(ClassUnderTest.class);
     }
-    
+
+    public static void start(Class<?> aClass) throws InvocationTargetException, IllegalAccessException {
+        Map<Integer, Method> map = new TreeMap<>();
+        Method beforeM = null;
+        Method afterM = null;
+        Method[] methods = aClass.getDeclaredMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                if (beforeM == null) {
+                    beforeM = m;
+                } else throw new RuntimeException("More than one annotated method @BeforeSuite");
+
+            } else if (m.isAnnotationPresent(Test.class)) {
+                int p = m.getAnnotation(Test.class).priority();
+                map.put(p, m);
+
+            } else if (m.isAnnotationPresent(AfterSuite.class)) {
+                if (afterM == null) {
+                    afterM = m;
+                } else throw new RuntimeException("More than one annotated method @AfterSuite");
+            }
+        }
+
+        if (beforeM != null) beforeM.invoke(null);
+        invokeMethodReverseMap(map);
+        if (afterM != null) afterM.invoke(null);
+    }
+
+    private static void invokeMethodReverseMap(Map<Integer, Method> map) throws IllegalAccessException, InvocationTargetException {
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        for (int i = keys.size() - 1; i >= 0; i--) {
+            map.get(keys.get(i)).invoke(null);
+        }
+    }
 }
